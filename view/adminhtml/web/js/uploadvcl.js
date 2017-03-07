3dabc0e524c82b42db2a1298b3c7dfe44d0eec16
Vladimir <vlemp@vuksan.com>
2017-03-07 11:20:41 -0500

    Since we are using .text injection we can't use HTML tags so I am gonna use ** instead.

--- view/adminhtml/web/js/uploadvcl.js
+++ view/adminhtml/web/js/uploadvcl.js
@@ -342,7 +342,7 @@ define([
             // Setting up label text
             setActiveServiceLabel: function (active_version, next_version, service_name) {
                 var msgWarning = $('.fastly-message-warning');
-                msgWarning.text($.mage.__('You are about to clone service <b>' + service_name + '</b> active version') + ' ' + active_version + '. '
+                msgWarning.text($.mage.__('You are about to clone service **' + service_name + '** active version') + ' ' + active_version + '. '
                     + $.mage.__('We\'ll make changes to version ') + ' ' + next_version + '.');
                 msgWarning.show();
             },

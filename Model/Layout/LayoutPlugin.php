4ee3d1e08bd931ecaef552b3596566fb55c2a232
Vladimir <vlemp@vuksan.com>
2017-04-26 15:50:39 -0400

    Bump version

--- Model/Layout/LayoutPlugin.php
+++ Model/Layout/LayoutPlugin.php
@@ -106,7 +106,7 @@ class LayoutPlugin
             }
             # Add a debug header to indicate this request has passed through the Fastly Module. This is
             # for ease of debugging
-            $this->response->setHeader("Fastly-Module-Enabled", "1.2.15", true);
+            $this->response->setHeader("Fastly-Module-Enabled", "1.2.16", true);
         }
         return $result;
     }

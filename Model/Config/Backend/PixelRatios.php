295861a4ce7bfda9c3e500c6d192c7df07a7f5a0
deninchoo <deni.pesic@inchoo.net>
2018-03-27 15:59:04 +0200

    Added field validation

--- Model/Config/Backend/PixelRatios.php
+++ Model/Config/Backend/PixelRatios.php
@@ -2,12 +2,15 @@
 
 namespace Fastly\Cdn\Model\Config\Backend;
 
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Exception\LocalizedException;
+
 /**
  * Class PixelRatios
  *
  * @package Fastly\Cdn\Model\Config\Backend
  */
-class PixelRatios
+class PixelRatios extends Value
 {
     /**
      * Options getter
@@ -23,5 +26,22 @@ class PixelRatios
             ['value' => '3.5', 'label'=>__('3.5x')],
             ['value' => '4', 'label'=>__('4x')]
         ];
+    }
+
+    /**
+     * @return $this|string
+     * @throws LocalizedException
+     */
+    public function beforeSave()
+    {
+        $value = $this->getValue();
+
+        if ($value === null || empty($value)) {
+            throw new LocalizedException(
+                __('At least one device pixel ratio must be selected.', $this->getFieldConfig('label'))
+            );
+        }
+
+        return $value;
     }
 }

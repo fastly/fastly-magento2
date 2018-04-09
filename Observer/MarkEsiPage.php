cf3318e2761e0b187513870ebe9de4442bad4f9a
deninchoo <deni.pesic@inchoo.net>
2018-04-09 14:39:34 +0200

    Replaced varnish check with fastly check

--- Observer/MarkEsiPage.php
+++ Observer/MarkEsiPage.php
@@ -10,6 +10,7 @@ use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\App\ResponseInterface as Response;
 use Magento\PageCache\Model\Config;
+use Fastly\Cdn\Model\Config as FastlyConfig;
 
 class MarkEsiPage implements ObserverInterface
 {
@@ -21,11 +22,9 @@ class MarkEsiPage implements ObserverInterface
     private $config;
 
     /**
-     * Is varnish enabled flag
-     *
-     * @var bool
+     * @var FastlyConfig
      */
-    private $isVarnishEnabled;
+    private $fastlyConfig;
 
     /**
      * @var EntitySpecificHandlesList
@@ -49,21 +48,23 @@ class MarkEsiPage implements ObserverInterface
 
     /**
      * MarkEsiPage constructor.
-     *
-     * @param \Magento\PageCache\Model\Config $config
+     * @param Config $config
+     * @param Response $response
+     * @param FastlyConfig $fastlyConfig
      * @param EntitySpecificHandlesList|null $entitySpecificHandlesList
      * @param Json|null $jsonSerializer
      * @param Base64Json|null $base64jsonSerializer
-     * @param Response $response
      */
-    public function __construct( // @codingStandardsIgnoreLine - required by parent class
+    public function __construct( // @codingStandardsIgnoreLine
         Config $config,
         Response $response,
+        FastlyConfig $fastlyConfig,
         EntitySpecificHandlesList $entitySpecificHandlesList = null,
         Json $jsonSerializer = null,
         Base64Json $base64jsonSerializer = null
     ) {
         $this->config = $config;
+        $this->fastlyConfig = $fastlyConfig;
         $this->entitySpecificHandlesList = $entitySpecificHandlesList
             ?: \Magento\Framework\App\ObjectManager::getInstance()->get(EntitySpecificHandlesList::class);
         $this->jsonSerializer = $jsonSerializer
@@ -74,19 +75,6 @@ class MarkEsiPage implements ObserverInterface
     }
 
     /**
-     * Is varnish cache engine enabled
-     *
-     * @return bool
-     */
-    private function isVarnishEnabled()
-    {
-        if ($this->isVarnishEnabled === null) {
-            $this->isVarnishEnabled = ($this->config->getType() == \Magento\PageCache\Model\Config::VARNISH);
-        }
-        return $this->isVarnishEnabled;
-    }
-
-    /**
      * Set x-esi header if block contains ttl attribute
      *
      * @param \Magento\Framework\Event\Observer $observer
@@ -94,17 +82,19 @@ class MarkEsiPage implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        $event = $observer->getEvent();
-        /** @var \Magento\Framework\View\Layout $layout */
-        $layout = $event->getLayout();
-        $name = $event->getElementName();
-        /** @var AbstractBlock $block */
-        $block = $layout->getBlock($name);
-        if ($block instanceof AbstractBlock) {
-            $blockTtl = $block->getTtl();
-            if (isset($blockTtl) && $this->isVarnishEnabled()) {
-                // This page potentially has ESIs so as a first cut let's mark it as such
-                $this->response->setHeader("x-esi", "1");
+        if ($this->fastlyConfig->isFastlyEnabled()) {
+            $event = $observer->getEvent();
+            /** @var \Magento\Framework\View\Layout $layout */
+            $layout = $event->getLayout();
+            $name = $event->getElementName();
+            /** @var AbstractBlock $block */
+            $block = $layout->getBlock($name);
+            if ($block instanceof AbstractBlock) {
+                $blockTtl = $block->getTtl();
+                if (isset($blockTtl)) {
+                    // This page potentially has ESIs so as a first cut let's mark it as such
+                    $this->response->setHeader("x-esi", "1");
+                }
             }
         }
     }

2613c8f9b39f9029067b71fc1c90096f05336007
deninchoo <deni.pesic@inchoo.net>
2018-05-08 08:31:03 +0200

    Added function to edit WAF_Response response object

---
+++ Block/System/Config/Form/Field/WafPageBtn.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Fastly\Cdn\Block\System\Config\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class WafPageBtn extends Field
+{
+    protected function _construct() // @codingStandardsIgnoreLine - required by parent class
+    {
+        $this->_template = 'Fastly_Cdn::system/config/form/field/wafPageBtn.phtml';
+
+        parent::_construct();
+    }
+
+    /**
+     * Remove scope label
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Return element html
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element) // @codingStandardsIgnoreLine - required by parent class
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for collect button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('adminhtml/fastlyCdn/vcl/serviceinfo');
+    }
+
+    /**
+     * Generate upload button html
+     *
+     * @return mixed
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'fastly_waf_page_button',
+                'label' => __('Edit WAF page'),
+            ]
+        );
+
+        return $button->toHtml();
+    }
+}

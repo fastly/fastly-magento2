98348d5728e9b633ed0e097919c0daf2ed0b0cd3
deninchoo <deni.pesic@inchoo.net>
2018-09-07 07:48:10 +0200

    Changed text

--- Controller/Adminhtml/FastlyCdn/Vcl/DeleteCustomSnippet.php
+++ Controller/Adminhtml/FastlyCdn/Vcl/DeleteCustomSnippet.php
@@ -84,7 +84,7 @@ class DeleteCustomSnippet extends Action
             $snippet = $this->getRequest()->getParam('snippet_id');
 
             $write = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
-            $snippetPath = $write->getrelativePath('vcl_snippets_custom/' . $snippet);
+            $snippetPath = $write->getRelativePath('vcl_snippets_custom/' . $snippet);
 
             if ($write->isExist($snippetPath)) {
                 $write->delete($snippetPath);

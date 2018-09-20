91aef07dc974f9f3840cc20e5e1891c9377dbc9a
V <vlemp@vuksan.com>
2018-09-20 11:21:58 -0400

    Fix for aspect ratio canvas when image is not square

--- Model/Product/Image.php
+++ Model/Product/Image.php
@@ -198,7 +198,7 @@ class Image extends ImageModel
         if ($this->_scopeConfig->isSetFlag(Config::XML_FASTLY_IMAGE_OPTIMIZATION_CANVAS) == true) {
             // Make sure Fastly delivers the specified size, even with letterboxing or pillarboxing.
             // We'll use aspect ratio canvas to avoid issues when using dpr
-            $this->fastlyParameters['canvas'] = "1:1";
+            $this->fastlyParameters['canvas'] = "{$this->_width}:{$this->_height}";
         }
     }
 

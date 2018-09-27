ee6168b3a03f00f918e7b1b1a87707e88e0b4e79
V <vlemp@vuksan.com>
2018-09-27 12:07:58 -0400

    Update version to 1.2.69

--- Model/Layout/LayoutPlugin.php
+++ Model/Layout/LayoutPlugin.php
@@ -111,7 +111,7 @@ class LayoutPlugin
     public function afterGetOutput(\Magento\Framework\View\Layout $subject, $result) // @codingStandardsIgnoreLine - unused parameter
     {
         if ($this->config->getType() == Config::FASTLY) {
-            $this->response->setHeader("Fastly-Module-Enabled", "1.2.68", true);
+            $this->response->setHeader("Fastly-Module-Enabled", "1.2.69", true);
         }
 
         return $result;

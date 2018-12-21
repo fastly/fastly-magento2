55ae25cbb963309904263be3d9cab23a6772af16
deninchoo <deni.pesic@inchoo.net>
2018-12-21 13:08:55 +0100

    Added some domain config files

---
+++ Controller/Adminhtml/FastlyCdn/Domains/PushDomains.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Fastly CDN for Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Fastly CDN for Magento End User License Agreement
+ * that is bundled with this package in the file LICENSE_FASTLY_CDN.txt.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Fastly CDN to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Fastly
+ * @package     Fastly_Cdn
+ * @copyright   Copyright (c) 2016 Fastly, Inc. (http://www.fastly.com)
+ * @license     BSD, see LICENSE_FASTLY_CDN.txt
+ */
+namespace Fastly\Cdn\Controller\Adminhtml\FastlyCdn\Domains;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Fastly\Cdn\Model\Config;
+use Fastly\Cdn\Model\Api;
+use Fastly\Cdn\Helper\Vcl;
+
+/**
+ * Class PushDomains
+ *
+ * @package Fastly\Cdn\Controller\Adminhtml\FastlyCdn\Domains
+ */
+class PushDomains extends Action
+{
+    /**
+     * @var Http
+     */
+    private $request;
+    /**
+     * @var JsonFactory
+     */
+    private $resultJson;
+    /**
+     * @var Config
+     */
+    private $config;
+    /**
+     * @var Api
+     */
+    private $api;
+    /**
+     * @var Vcl
+     */
+    private $vcl;
+
+    /**
+     * PushDomains constructor.
+     *
+     * @param Context $context
+     * @param Http $request
+     * @param JsonFactory $resultJsonFactory
+     * @param Config $config
+     * @param Api $api
+     * @param Vcl $vcl
+     */
+    public function __construct(
+        Context $context,
+        Http $request,
+        JsonFactory $resultJsonFactory,
+        Config $config,
+        Api $api,
+        Vcl $vcl
+    ) {
+        $this->request = $request;
+        $this->resultJson = $resultJsonFactory;
+        $this->config = $config;
+        $this->api = $api;
+        $this->vcl = $vcl;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Json|\Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $result = $this->resultJson->create();
+
+        try {
+            $activeVersion = $this->getRequest()->getParam('active_version');
+            $activateVcl = $this->getRequest()->getParam('activate_flag');
+            $currentDomains = $this->getRequest()->getParam('current_domains');
+            $newDomains = $this->getRequest()->getParam('domains');
+            $service = $this->api->checkServiceDetails();
+            $this->vcl->checkCurrentVersionActive($service->versions, $activeVersion);
+            $currActiveVersion = $this->vcl->getCurrentVersion($service->versions);
+            $clone = $this->api->cloneVersion($currActiveVersion);
+            $params = [
+                'name'      => $name,
+                'comment'   => $comment
+            ];
+
+            if (!createDomain) {
+                return $result->setData([
+                    'status'    => false,
+                    'msg'       => 'Failed to push domains.'
+                ]);
+            }
+
+            $this->api->validateServiceVersion($clone->number);
+
+            if ($activateVcl === 'true') {
+                $this->api->activateVersion($clone->number);
+            }
+
+            $comment = ['comment' => 'Magento Module pushed domains'];
+            $this->api->addComment($clone->number, $comment);
+
+            return $result->setData([
+                'status'            => true,
+                'active_version'    => $clone->number
+            ]);
+        } catch (\Exception $e) {
+            return $result->setData([
+                'status'    => false,
+                'msg'       => $e->getMessage()
+            ]);
+        }
+    }
+}

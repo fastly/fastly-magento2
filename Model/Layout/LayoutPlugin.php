924dd3aed693af47886c4d763f4c97a59f08a5a1
Vladimir Vuksan <vlemp@vuksan.com>
2019-02-15 13:44:33 -0500

    Bump to 1.2.84

--- Model/Layout/LayoutPlugin.php
+++ Model/Layout/LayoutPlugin.php
@@ -111,7 +111,7 @@ class LayoutPlugin
     public function afterGetOutput(\Magento\Framework\View\Layout $subject, $result) // @codingStandardsIgnoreLine - unused parameter
     {
         if ($this->config->getType() == Config::FASTLY) {
-            $this->response->setHeader("Fastly-Module-Enabled", "1.2.83", true);
+            $this->response->setHeader("Fastly-Module-Enabled", "1.2.84", true);
         }
 
         return $result;

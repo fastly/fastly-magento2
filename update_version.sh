8d90d5bb6913550a09b842cc859bdf16c87162ce
Vladimir Vuksan <vlemp@vuksan.com>
2019-07-19 08:54:14 -0400

    Send uploaded VCL version to the backend

--- update_version.sh
+++ update_version.sh
@@ -3,5 +3,6 @@
 VERSION=`cat VERSION`
 
 sed -i "s/resp.http.Fastly-Magento-VCL-Uploaded = \".*\"/resp.http.Fastly-Magento-VCL-Uploaded = \"$VERSION\"/g" etc/vcl_snippets/*.vcl
+sed -i "s/req.http.Fastly-Magento-VCL-Uploaded = \".*\"/req.http.Fastly-Magento-VCL-Uploaded = \"$VERSION\"/g" etc/vcl_snippets/*.vcl
 sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" composer.json
 sed -i "s/\"Fastly-Module-Enabled\", \".*\"/\"Fastly-Module-Enabled\", \"$VERSION\"/g" Model/Layout/LayoutPlugin.php

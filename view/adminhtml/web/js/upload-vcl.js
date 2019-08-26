736198260e2869cb1bc4769c97721738d04c9072
lukas.scharmitzer <lukas.scharmitzer@inchoo.net>
2019-08-26 11:03:54 +0200

    coding standard

--- view/adminhtml/web/js/upload-vcl.js
+++ view/adminhtml/web/js/upload-vcl.js
@@ -1,4 +1,5 @@
-define([
+define(
+    [
     "jquery",
     "setServiceLabel",
     "overlay",
@@ -6,167 +7,196 @@ define([
     "showErrorMessage",
     "Magento_Ui/js/modal/confirm",
     'mage/translate'
-], function ($, setServiceLabel, overlay, resetAllMessages, showErrorMessage, confirm) {
-    return function (config, serviceStatus, isAlreadyConfigured) {
-        /* VCL button messages */
-        let successVclBtnMsg = $('#fastly-success-vcl-button-msg');
-        let errorVclBtnMsg = $('#fastly-error-vcl-button-msg');
-        let outdatedErrorMsg = $("#fastly-warning-outdated-vcl-button-msg");
-        let active_version = serviceStatus.active_version;
+    ], function ($, setServiceLabel, overlay, resetAllMessages, showErrorMessage, confirm) {
+        return function (config, serviceStatus, isAlreadyConfigured) {
+            /* VCL button messages */
+            let successVclBtnMsg = $('#fastly-success-vcl-button-msg');
+            let errorVclBtnMsg = $('#fastly-error-vcl-button-msg');
+            let outdatedErrorMsg = $("#fastly-warning-outdated-vcl-button-msg");
+            let active_version = serviceStatus.active_version;
 
-        $(document).ready(function () {
-            isWarningDismissed(active_version);
-            let uploadOptions = {
-                title: jQuery.mage.__('You are about to upload VCL to Fastly '),
-                content: function () {
-                    return document.getElementById('fastly-uploadvcl-template').textContent;
-                },
-                actionOk: function () {
-                    uploadVcl(active_version);
-                }
-            };
+            $(document).ready(
+                function () {
+                    isWarningDismissed(active_version);
+                    let uploadOptions = {
+                        title: jQuery.mage.__('You are about to upload VCL to Fastly '),
+                        content: function () {
+                            return document.getElementById('fastly-uploadvcl-template').textContent;
+                        },
+                        actionOk: function () {
+                            uploadVcl(active_version);
+                        }
+                    };
 
-            function isWarningDismissed(activeVersion)
-            {
-                $.ajax({
-                    type: 'GET',
-                    url: config.isWarningDismissed,
-                    data: {active_version: activeVersion},
-                    showLoader: false,
-                    success: function (response) {
-                        if(response.status !== false){
+                    function isWarningDismissed(activeVersion)
+                    {
+                        $.ajax(
+                            {
+                                type: 'GET',
+                                url: config.isWarningDismissed,
+                                data: {active_version: activeVersion},
+                                showLoader: false,
+                                success: function (response) {
+                                    if (response.status !== false) {
+                                        resetAllMessages();
+                                        if (response.dismissed !== true) {
+                                            compareVclVersions(activeVersion);
+                                            return;
+                                        }
+
+                                        return;
+                                    }
+
+                                    return errorVclBtnMsg.text($.mage.__(response.msg)).show();
+                                }
+                            }
+                        )
+                    }
+
+                    function compareVclVersions()
+                    {
+                        $.ajax(
+                            {
+                                type: 'GET',
+                                url: config.vclComparison,
+                                showLoader: false,
+                                data: {'active_version':active_version},
+                                success: function (response) {
+                                    if (response.status !== true) {
+                                        let span = document.createElement('span');
+                                        span.setAttribute('class', 'fastly-dismiss-warning-action');
+                                        span.setAttribute('title', 'Dismiss Warning');
+                                        outdatedErrorMsg.text($.mage.__(response.msg)).show();
+                                        outdatedErrorMsg.append(span);
+                                        openDismissModal();
+                                    }
+                                }
+                            }
+                        );
+                    }
+
+                    /**
+                     * VCL Upload button on click event
+                     */
+                    $('#fastly_vcl_upload_button').on(
+                        'click', function () {
+                            if (isAlreadyConfigured !== true) {
+                                $(this).attr('disabled', true);
+                                return alert($.mage.__('Please save config prior to continuing.'));
+                            }
+
                             resetAllMessages();
-                            if(response.dismissed !== true){
-                                compareVclVersions(activeVersion);
-                                return;
-                            }
-                            return;
+
+                            $.when(
+                                $.ajax(
+                                    {
+                                        type: "GET",
+                                        url: config.serviceInfoUrl,
+                                        showLoader: true
+                                    }
+                                )
+                            ).done(
+                                function (service) {
+                                    if (service.status === false) {
+                                        return errorVclBtnMsg.text($.mage.__('Please check your Service ID and API token and try again.')).show();
+                                    }
+
+                                    active_version = service.active_version;
+                                    let next_version = service.next_version;
+                                    let service_name = service.service.name;
+                                    overlay(uploadOptions);
+                                    setServiceLabel(active_version, next_version, service_name);
+
+                                }
+                            ).fail(
+                                function () {
+                                    return errorVclBtnMsg.text($.mage.__('An error occurred while processing your request. Please try again.')).show();
+                                }
+                            );
                         }
-                        return errorVclBtnMsg.text($.mage.__(response.msg)).show();
-                    }
-                })
-            }
+                    );
 
-            function compareVclVersions()
-            {
-                $.ajax({
-                   type: 'GET',
-                   url: config.vclComparison,
-                   showLoader: false,
-                   data: {'active_version':active_version},
-                   success: function (response) {
-                      if(response.status !== true){
-                          let span = document.createElement('span');
-                          span.setAttribute('class', 'fastly-dismiss-warning-action');
-                          span.setAttribute('title', 'Dismiss Warning');
-                          outdatedErrorMsg.text($.mage.__(response.msg)).show();
-                          outdatedErrorMsg.append(span);
-                          openDismissModal();
-                      }
-                   }
-                });
-            }
+                    /**
+                     * Upload VCL snippets to the Fastly service
+                     */
+                    function uploadVcl()
+                    {
+                        let activate_vcl_flag = false;
 
-            /**
-             * VCL Upload button on click event
-             */
-            $('#fastly_vcl_upload_button').on('click', function () {
-                if (isAlreadyConfigured !== true) {
-                    $(this).attr('disabled', true);
-                    return alert($.mage.__('Please save config prior to continuing.'));
-                }
-
-                resetAllMessages();
-
-                $.when(
-                    $.ajax({
-                        type: "GET",
-                        url: config.serviceInfoUrl,
-                        showLoader: true
-                    })
-                ).done(function (service) {
-                    if (service.status === false) {
-                        return errorVclBtnMsg.text($.mage.__('Please check your Service ID and API token and try again.')).show();
-                    }
-
-                    active_version = service.active_version;
-                    let next_version = service.next_version;
-                    let service_name = service.service.name;
-                    overlay(uploadOptions);
-                    setServiceLabel(active_version, next_version, service_name);
-
-                }).fail(function () {
-                    return errorVclBtnMsg.text($.mage.__('An error occurred while processing your request. Please try again.')).show();
-                });
-            });
-
-            /**
-             * Upload VCL snippets to the Fastly service
-             */
-            function uploadVcl()
-            {
-                let activate_vcl_flag = false;
-
-                if ($('#fastly_activate_vcl').is(':checked')) {
-                    activate_vcl_flag = true;
-                }
-
-                $.ajax({
-                    type: "POST",
-                    url: config.vclUploadUrl,
-                    data: {
-                        'activate_flag': activate_vcl_flag,
-                        'active_version': active_version
-                    },
-                    showLoader: true
-                }).done(function (response) {
-                    if (response.status === true) {
-                        modal.modal('closeModal');
-                        successVclBtnMsg.text($.mage.__('VCL file is successfully uploaded to the Fastly service.')).show();
-                    } else {
-                        resetAllMessages();
-                        showErrorMessage(response.msg);
-                    }
-                });
-            }
-
-            function openDismissModal()
-            {
-                $("#fastly-warning-outdated-vcl-button-msg").on('hover', function(){
-                   $(this).css('cursor', 'pointer');
-                });
-
-                $("#fastly-warning-outdated-vcl-button-msg").on('click', function () {
-                    confirm({
-                        title: 'Dismiss outdated VCL warning',
-                        content: 'Are you sure you want to dismiss warning for the current version #<b>' + active_version + '</b> ?',
-                        actions: {
-                            confirm: function () {
-                                dismissWarning(active_version);
-                            },
-                            cancel: function () {
-                            }
+                        if ($('#fastly_activate_vcl').is(':checked')) {
+                            activate_vcl_flag = true;
                         }
-                    });
-                });
 
-                function dismissWarning(version)
-                {
-                    $.ajax({
-                       type: 'GET',
-                       url: config.dismissWarning,
-                       showLoader: true,
-                       data: {active_version: version},
-                       success: function (response) {
-                           if(response.status !== false){
-                               resetAllMessages();
-                               return successVclBtnMsg.text($.mage.__(response.msg)).show();
-                           }
-                           return errorVclBtnMsg.text($.mage.__(response.msg)).show();
-                       }
-                    });
+                        $.ajax(
+                            {
+                                type: "POST",
+                                url: config.vclUploadUrl,
+                                data: {
+                                    'activate_flag': activate_vcl_flag,
+                                    'active_version': active_version
+                                },
+                                showLoader: true
+                            }
+                        ).done(
+                            function (response) {
+                                if (response.status === true) {
+                                    modal.modal('closeModal');
+                                    successVclBtnMsg.text($.mage.__('VCL file is successfully uploaded to the Fastly service.')).show();
+                                } else {
+                                    resetAllMessages();
+                                    showErrorMessage(response.msg);
+                                }
+                            }
+                        );
+                    }
+
+                    function openDismissModal()
+                    {
+                        $("#fastly-warning-outdated-vcl-button-msg").on(
+                            'hover', function () {
+                                $(this).css('cursor', 'pointer');
+                            }
+                        );
+
+                        $("#fastly-warning-outdated-vcl-button-msg").on(
+                            'click', function () {
+                                confirm(
+                                    {
+                                        title: 'Dismiss outdated VCL warning',
+                                        content: 'Are you sure you want to dismiss warning for the current version #<b>' + active_version + '</b> ?',
+                                        actions: {
+                                            confirm: function () {
+                                                dismissWarning(active_version);
+                                            },
+                                            cancel: function () {
+                                            }
+                                        }
+                                    }
+                                );
+                            }
+                        );
+
+                        function dismissWarning(version)
+                        {
+                            $.ajax(
+                                {
+                                    type: 'GET',
+                                    url: config.dismissWarning,
+                                    showLoader: true,
+                                    data: {active_version: version},
+                                    success: function (response) {
+                                        if(response.status !== false) {
+                                            resetAllMessages();
+                                            return successVclBtnMsg.text($.mage.__(response.msg)).show();
+                                        }
+                                        return errorVclBtnMsg.text($.mage.__(response.msg)).show();
+                                    }
+                                }
+                            );
+                        }
+                    }
                 }
-            }
-        });
+            );
+        }
     }
-});
+);

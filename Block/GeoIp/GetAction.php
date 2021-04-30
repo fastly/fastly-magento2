dadfa5df27c9de3f210b47548dc7eec8948cdbbd
Vladyslav Sikailo <dren7777@gmail.com>
2021-04-30 13:19:15 +0300

    Add upercase for url path

--- Block/GeoIp/GetAction.php
+++ Block/GeoIp/GetAction.php
@@ -87,7 +87,7 @@ class GetAction extends AbstractBlock
         }
 
         /** @var string $actionUrl */
-        $actionUrl = $this->getUrl('fastlyCdn/geoip/getaction');
+        $actionUrl = $this->getUrl('fastlyCdn/geoIp/getAction');
         $vclUploaded = $this->_request->getServer('HTTP_FASTLY_MAGENTO_VCL_UPLOADED');
         $currentUrl = $this->url->getCurrentUrl();
         $baseUrl = $this->url->getBaseUrl();

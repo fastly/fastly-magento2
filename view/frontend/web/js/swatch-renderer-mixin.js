730ccb0bf25e95eec301559319de3ed83d44962c
kirgiv4oja <lukas.scharmitzer@gmail.com>
2021-06-07 08:39:05 +0200

    changed mixin to mixing the widget

--- view/frontend/web/js/swatch-renderer-mixin.js
+++ view/frontend/web/js/swatch-renderer-mixin.js
@@ -1,37 +1,41 @@
 define([
-    'mage/utils/wrapper',
+    'jquery',
     'underscore'
-], function (wrapper, _) {
+], function ($, _) {
     'use strict';
 
     return function (swatchRenderer) {
 
-        return wrapper.wrap(swatchRenderer, function (create, config, element) {
+        $.widget('mage.SwatchRenderer', swatchRenderer, {
+            
+            _init: function () {
+                if (_.isUndefined(this.options) || _.isEmpty(this.options))
+                        return this._super();
 
-            if (_.isUndefined(config) || _.isEmpty(config))
-                return create(config, element);
+                if (_.isUndefined(this.options.jsonConfig) || _.isEmpty(this.options.jsonConfig))
+                    return this._super();
 
-            if (_.isUndefined(config.jsonConfig) || _.isEmpty(config.jsonConfig))
-                return create(config, element);
+                if (_.isUndefined(this.options.jsonConfig.images) || _.isEmpty(this.options.jsonConfig.images))
+                    return this._super();
 
-            if (_.isUndefined(config.jsonConfig.images) || _.isEmpty(config.jsonConfig.images))
-                return create(config, element);
+                let wdpr = window.devicePixelRatio;
 
-            let wdpr = window.devicePixelRatio;
+                _.each(this.options.jsonConfig.images, function (images) {
+                    _.each(images, function (imageObject) {
+                        if (_.isUndefined(imageObject.fastly_srcset))
+                            return;
 
-            _.each(config.jsonConfig.images, function (images) {
-                _.each(images, function (imageObject) {
-                    if (_.isUndefined(imageObject.fastly_srcset))
-                        return;
+                        if (!_.has(imageObject.fastly_srcset, wdpr))
+                            return;
 
-                    if (!_.has(imageObject.fastly_srcset, wdpr))
-                        return;
-
-                    imageObject.img = imageObject.fastly_srcset[wdpr];
+                        imageObject.img = imageObject.fastly_srcset[wdpr];
+                    });
                 });
-            });
 
-            create(config, element);
+                return this._super();
+            }
         });
+
+        return $.mage.SwatchRenderer;
     };
 });

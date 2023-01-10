589df2b40fc9aab0f6b1ca5b969e9d6537118446
Borna Butkovic <borna@favicode.net>
2023-01-10 10:10:37 +0100

    implement dynamic srcset on swatches

--- view/frontend/web/js/swatch-renderer-mixin.js
+++ view/frontend/web/js/swatch-renderer-mixin.js
@@ -7,6 +7,13 @@ define([
     return function (swatchRenderer) {
 
         $.widget('mage.SwatchRenderer', swatchRenderer, {
+            lastImageResponse: null,
+
+            imageSizes: [
+                'large',
+                'medium',
+                'small',
+            ],
 
             _init: function () {
                 if (_.isUndefined(this.options) || _.isEmpty(this.options))
@@ -34,10 +41,69 @@ define([
 
                 return this._super();
             },
-            updateBaseImage: function (images, context, isInProductView){
+
+            _ProductMediaCallback: function ($this, response, isInProductView) {
+                this.lastImageResponse = response;
+                return this._super($this, response, isInProductView);
+            },
+
+            updateBaseImage: function (images, context, isInProductView) {
                 this._super(images, context, isInProductView);
-                //add logic for srcset
-                //context.find('.product-image-photo').attr('srcset', justAnImage.img);
+                if (isInProductView || !this.lastImageResponse) {
+                    return;
+                }
+
+                var currentSrc = context.find('.product-image-photo').attr('src');
+                if (!currentSrc) {
+                    return;
+                }
+
+                var srcset = this.findEquivalentSrcset(currentSrc);
+                if (srcset) {
+                    context.find('.product-image-photo').attr('srcset', srcset);
+                }
+            },
+
+            findEquivalentSrcset: function (src) {
+                if (!this.lastImageResponse || !this.lastImageResponse.fastly_srcset) {
+                    return null;
+                }
+
+                var srcset = this.findSrcsetInImageResponse(src, this.lastImageResponse);
+                if (srcset) {
+                    return srcset;
+                }
+
+                var gallery = this.lastImageResponse.gallery;
+                if (!gallery) {
+                    return null;
+                }
+
+                for (var i in gallery) {
+                    if (!gallery.hasOwnProperty(i)) {
+                        continue;
+                    }
+
+                    srcset = this.findFromImageList(src, gallery[i]);
+                    if (srcset) {
+                        return srcset;
+                    }
+                }
+
+                return null;
+            },
+
+            findSrcsetInImageResponse: function (src, list) {
+                if (!list.fastly_srcset) {
+                    return null;
+                }
+
+                this.imageSizes.forEach(function (size) {
+                    if (list[size] && list[size] === src) {
+                        return list.fastly_srcset[size];
+                    }
+                });
+
             }
         });
 

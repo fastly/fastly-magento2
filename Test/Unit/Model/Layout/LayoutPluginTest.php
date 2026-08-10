f0c3358061b0b2d9559eb9860ddacb3a1e34b70a
ivanviduka <viduka.ivan@gmail.com>
2026-08-10 14:33:11 +0200

    Deprecated methods fix

--- Test/Unit/Model/Layout/LayoutPluginTest.php
+++ Test/Unit/Model/Layout/LayoutPluginTest.php
@@ -198,45 +198,49 @@ class LayoutPluginTest extends TestCase
     /**
      * @return array[]
      */
-     public function afterGenerateElementsDataProvider(): array
-     {
-         return [
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL > 0, StaleErrorTTL > 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 1],
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL > 0, StaleErrorTTL = 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 0],
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL = 0, StaleErrorTTL > 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 0, 1],
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL = 0, StaleErrorTTL = 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 0, 0],
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL = 0' =>
-                 [true, true,  \Fastly\Cdn\Model\Config::FASTLY, 0],
-             'Full_cache state is true, Layout is cache-able, Varnish, TTL > 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::VARNISH, 1],
-             'Full_cache state is true, Layout is cache-able, Varnish, TTL = 0' =>
-                 [true, true, \Fastly\Cdn\Model\Config::VARNISH, 0],
-             'Full_cache state is true, Layout is not cache-able, Fastly, TTL > 0' =>
-                 [true, false, \Fastly\Cdn\Model\Config::FASTLY, 1],
-             'Full_cache state is false, Layout is not cache-able, Fastly, TTL > 0' =>
-                 [false, false, \Fastly\Cdn\Model\Config::FASTLY, 1],
-             'Full_cache state is false, Layout is cache-able, Fastly, TTL > 0' =>
-                 [false, true, \Fastly\Cdn\Model\Config::FASTLY, 1],
-             'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, cache-control empty' =>
-                 [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 1, ''],
-         ];
-     }
+    public static function afterGenerateElementsDataProvider(): array
+    {
+        return [
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL > 0, StaleErrorTTL > 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 1],
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL > 0, StaleErrorTTL = 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 0],
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL = 0, StaleErrorTTL > 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 0, 1],
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, StaleTTL = 0, StaleErrorTTL = 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 0, 0],
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL = 0' =>
+                [true, true,  \Fastly\Cdn\Model\Config::FASTLY, 0],
+            'Full_cache state is true, Layout is cache-able, Varnish, TTL > 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::VARNISH, 1],
+            'Full_cache state is true, Layout is cache-able, Varnish, TTL = 0' =>
+                [true, true, \Fastly\Cdn\Model\Config::VARNISH, 0],
+            'Full_cache state is true, Layout is not cache-able, Fastly, TTL > 0' =>
+                [true, false, \Fastly\Cdn\Model\Config::FASTLY, 1],
+            'Full_cache state is false, Layout is not cache-able, Fastly, TTL > 0' =>
+                [false, false, \Fastly\Cdn\Model\Config::FASTLY, 1],
+            'Full_cache state is false, Layout is cache-able, Fastly, TTL > 0' =>
+                [false, true, \Fastly\Cdn\Model\Config::FASTLY, 1],
+            'Full_cache state is true, Layout is cache-able, Fastly, TTL > 0, cache-control empty' =>
+                [true, true, \Fastly\Cdn\Model\Config::FASTLY, 1, 1, 1, ''],
+        ];
+    }
 
      /**
       * @param $configCacheType
-      * @param $cntGetHeader
       * @param $headerName
-      * @param $cacheControlHeader
       * @param $cntSetHeader
       * @dataProvider afterGetOutputDataProvider
       */
-     public function testAfterGetOutput($configCacheType, $cntGetHeader, $headerName, $cacheControlHeader, $cntSetHeader)
+     public function testAfterGetOutput($configCacheType, $headerName, $cntSetHeader)
      {
          $html = 'html';
+
+         if ($cntSetHeader === 1) {
+             $cntSetHeader = $this->once();
+         } else {
+             $cntSetHeader = $this->never();
+         }
 
          $this->configMock->expects($this->once()
          )->method('getType'
@@ -253,19 +257,17 @@ class LayoutPluginTest extends TestCase
     /**
      * @return array[]
      */
-     public function afterGetOutputDataProvider(): array
-     {
-         $headerName = 'Fastly-Module-Enabled';
+    public static function afterGetOutputDataProvider(): array
+    {
+        $headerName = 'Fastly-Module-Enabled';
 
-         $cacheControlHeader = new GenericHeader($headerName, $this->moduleVersion);
-
-         return [
-             'Fastly, getHeader: Yes, setHeader: Yes' =>
-                 [\Fastly\Cdn\Model\Config::FASTLY, $this->once(), $headerName,$cacheControlHeader, $this->once()],
-             'Fastly, getHeader: Yes, setHeader: Yes (updated)' =>
-                 [\Fastly\Cdn\Model\Config::FASTLY, $this->once(), $headerName,false, $this->once()],
-             'Varnish, getHeader: No, setHeader: No' =>
-                 [Config::VARNISH, $this->never(), $headerName, false, $this->never()]
-         ];
-     }
+        return [
+            'Fastly, getHeader: Yes, setHeader: Yes' =>
+                [\Fastly\Cdn\Model\Config::FASTLY, $headerName, 1],
+            'Fastly, getHeader: Yes, setHeader: Yes (updated)' =>
+                [\Fastly\Cdn\Model\Config::FASTLY, $headerName, 1],
+            'Varnish, getHeader: No, setHeader: No' =>
+                [Config::VARNISH, $headerName, 0]
+        ];
+    }
 }
